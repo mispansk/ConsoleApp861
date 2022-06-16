b4e681d26b78b3a442ee1244e8b9b3bd45905075
mispansk <mispansk@gmail.com>
2022-06-17 02:34:30 +0700

    Добавьте файлы проекта.

---
+++ ConsoleApp861/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp861
+{
+    class Program
+    {
+     
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var dirName = new DirectoryInfo(@"C:\Work\skillFactori\123"); // Прописываем путь к директории 
+
+            if (dirName.Exists)
+            {
+                bool res = MyWorkWithFiles.DeleteDirByTime(dirName);
+                // если res = true , тогда папка пустая (изначальная , которую задавали), т.е. если нужно, то можно прописать удаление...  
+            }
+            else
+            {
+                Console.WriteLine("Некорректно указан путь");
+            }
+            Console.ReadKey();
+        } 
+    }
+}
